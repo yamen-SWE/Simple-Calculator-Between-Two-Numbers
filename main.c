30924f201d13c4573ac01d55d16f9c4802485bfb
Rant <yamen74653@gmail.com>
2026-05-06 15:53:27 +0300

    A simple calculator for adding two numbers

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int num1,num2,sum ;
+    printf("enter a chooes (1) : ");
+    scanf("%d",&num1);
+    printf("enetr a chooes (2) : ");
+    scanf("%d",&num2);
+    sum=num1+num2;
+    printf("the sum is %d\n",sum);;
+    return 0;
+}
